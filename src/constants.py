1b84f167147e1e385723f1cbb24aae34a9185a49
Caeserus <122463874+Caeserus@users.noreply.github.com>
2023-01-11 17:25:13 +0000

    Modifications de modération et personnalité

--- src/constants.py
+++ src/constants.py
@@ -37,31 +37,31 @@ for s in server_channels:
 BOT_INVITE_URL = f"https://discord.com/api/oauth2/authorize?client_id={DISCORD_CLIENT_ID}&permissions=328565073920&scope=bot"
 
 MODERATION_VALUES_FOR_BLOCKED = {
-    "hate": 0.5,
-    "hate/threatening": 0.1,
-    "self-harm": 0.2,
-    "sexual": 0.5,
-    "sexual/minors": 0.2,
-    "violence": 0.7,
-    "violence/graphic": 0.8,
+    "hate": 1,
+    "hate/threatening": 1,
+    "self-harm": 1,
+    "sexual": 1,
+    "sexual/minors": 1,
+    "violence": 1,
+    "violence/graphic": 1,
 }
 
 MODERATION_VALUES_FOR_FLAGGED = {
-    "hate": 0.4,
-    "hate/threatening": 0.05,
-    "self-harm": 0.1,
-    "sexual": 0.3,
-    "sexual/minors": 0.1,
-    "violence": 0.1,
-    "violence/graphic": 0.1,
+    "hate": 1,
+    "hate/threatening": 1,
+    "self-harm": 1,
+    "sexual": 1,
+    "sexual/minors": 1,
+    "violence": 1,
+    "violence/graphic": 1,
 }
 
 SECONDS_DELAY_RECEIVING_MSG = (
     3  # give a delay for the bot to respond so it can catch multiple messages
 )
-MAX_THREAD_MESSAGES = 200
+MAX_THREAD_MESSAGES = 10000
 ACTIVATE_THREAD_PREFX = "💬✅"
 INACTIVATE_THREAD_PREFIX = "💬❌"
 MAX_CHARS_PER_REPLY_MSG = (
-    1500  # discord has a 2k limit, we just break message into 1.5k
+    2000  # discord has a 2k limit, we just break message into 1.5k
 )
